6397f919a8c0861e9477341e2468acf95f0edc47
Steve <steve21johnson@gmail.com>
2022-12-28 13:28:48 -0500

    0

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Dot/>
-      <Beam/>
-      <div style={{display: 'flex', marginTop: 100,}}>
-        <div className={'dot'}/>
-        <div className={'dot'}/>
-        <div className={'dot'}/>
-        <div className={'dot'}/>
-      </div>
+      <Dot loc={teachLoc}/>
+      <Beam from={teachLoc} to={s0Loc}/>
+      <Dot loc={s0Loc}/>
     </div>
   );
 }
-function Dot() {
-  return <div className={'dot'}/>
+const teachLoc: Loc = [0, 0]
+const s0Loc: Loc = [0, 100]
+function Dot({loc}: {loc: Loc}) {
+  return <div style={{left: loc[0], top: loc[1]}} className={'dot'}/>
 }
-type coord = [number, number]
-function Beam({from, to}: {from?: coord, to?: coord}) {
-  return <div className={'beam'}/>
+type Loc = [number, number]
+function Beam({from, to}: {from: Loc, to: Loc}) {
+  return <div style={{"--beamStart": from[1] + "px", "--beamEnd": to[1] + "px"} as any} className={'beam'}/>
 }
+
 export default App;
