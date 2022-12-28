414cbdf622b65b696706eb5b6f069b052d7ac92e
Steve <steve21johnson@gmail.com>
2022-12-28 12:58:54 -0500

    0

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Dot/>
 
+      <div style={{display: 'flex', marginTop: 100,}}>
+        <div className={'dot'}/>
+        <div className={'dot'}/>
+        <div className={'dot'}/>
+        <div className={'dot'}/>
+      </div>
     </div>
   );
 }
-
+function Dot() {
+  return <div className={'dot'}/>
+}
+function Beam() {
+  return <div style={{position: "absolute"}}/>
+}
 export default App;
