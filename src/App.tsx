a56afc009b55e3ce4c86e235e165d8e3fefb5f9a
Steve <steve21johnson@gmail.com>
2022-12-28 13:36:39 -0500

    prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Dot loc={teachLoc}/>
-      <Beam from={teachLoc} to={s0Loc}/>
-      <Dot loc={s0Loc}/>
+      <Dot loc={teachLoc} />
+      {studentLocs.map((sl) => (
+        <>
+          <Beam from={teachLoc} to={sl} />
+          <Dot loc={sl} />
+        </>
+      ))}
     </div>
   );
 }
-const teachLoc: Loc = [0, 0]
-const s0Loc: Loc = [0, 100]
-function Dot({loc}: {loc: Loc}) {
-  return <div style={{left: loc[0], top: loc[1]}} className={'dot'}/>
+const teachLoc: Loc = [0, 0];
+const studentLocs: Loc[] = Array(4)
+  .fill(null)
+  .map((v, i) => [i * 100, 100]);
+function Dot({ loc }: { loc: Loc }) {
+  return <div style={{ left: loc[0], top: loc[1] }} className={"dot"} />;
 }
-type Loc = [number, number]
-function Beam({from, to}: {from: Loc, to: Loc}) {
-  return <div style={{"--beamStart": from[1] + "px", "--beamEnd": to[1] + "px"} as any} className={'beam'}/>
+type Loc = [number, number];
+function Beam({ from, to }: { from: Loc; to: Loc }) {
+  return (
+    <div
+      style={
+        { "--beamStart": from[1] + "px", "--beamEnd": to[1] + "px" } as any
+      }
+      className={"beam"}
+    />
+  );
 }
 
 export default App;
